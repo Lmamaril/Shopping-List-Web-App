721d72dc02eddd3757aabeaa82180f2582ed4fb8
Leah Mamaril <8636027+Lmamaril@users.noreply.github.com>
2020-02-11 10:28:50 -0800

    Added card wrapped around list

--- script.js
+++ script.js
@@ -12,18 +12,19 @@ function addButton(li){
 
 addBtn.addEventListener('click', ()=>{
     let item = input.value;
-    let li = document.createElement('li');
-    li.className = 'd-flex justify-content-between px-5 mx-5 py-2';
-    li.textContent = item;
-    addButton(li)
-    listItems.appendChild(li);
-    input.value = '';
+    if (input.value.length != 0) {
+        let li = document.createElement('li');
+        li.className = 'd-flex justify-content-between px-5 mx-5 py-2';
+        li.textContent = item;
+        addButton(li)
+        listItems.appendChild(li);
+        input.value = '';
+    }
 });
 
 listItems.addEventListener('click', (event) =>{
     if (event.target.tagName == 'BUTTON') {
         let li = event.target.parentNode;
-        let ul = li.parentNode;
-        ul.removeChild(li);
+        listItems.removeChild(li);
     }
 })
